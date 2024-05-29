f603bf61c63d3cc168c3c701e5663438db45df45
Bernardo Ferrão <143981716+BernardoGFerrao@users.noreply.github.com>
2024-05-29 18:45:40 -0300

    Using HTML templates on our pages

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 #Criando uma rota(Caminho para o link do site)( / -> Homepage)
 def homepage():
-    return 'Fake Pinterest - Home page'
+    return render_template('homepage.html')
+
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
