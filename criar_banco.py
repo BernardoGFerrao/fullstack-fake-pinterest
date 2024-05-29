6ffa47dff58774a91220a82d292e902b9c691aed
Bernardo Ferrão <143981716+BernardoGFerrao@users.noreply.github.com>
2024-05-29 20:22:11 -0300

    update

--- criar_banco.py
+++
@@ -1,7 +0,0 @@
-#Criar o banco de dados:
-from fakepinterest import database, app
-from fakepinterest.models import Usuario, Post
-
-#Cria um banco SQL
-with app.app_context():
-    database.create_all()
