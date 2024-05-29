64d43dbd60f4dded6fa0a5fec8cc7b8a38fe230d
Bernardo Ferrão <143981716+BernardoGFerrao@users.noreply.github.com>
2024-05-29 19:08:51 -0300

    Routes and Dynamic Pages

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -7,9 +7,10 @@ app = Flask(__name__)
 def homepage():
     return render_template('homepage.html')
 
-@app.route('/perfil')
-def perfil():
-    return render_template('perfil.html')
+#Para não precisar criar uma rota para cada pessoa -> Páginas dinâmicas
+@app.route(f'/perfil/<usuario>')
+def perfil(usuario):
+    return render_template('perfil.html', usuario=usuario)#render template permite conversar com o html
 
 if __name__ == '__main__':
     app.run(debug=True)
