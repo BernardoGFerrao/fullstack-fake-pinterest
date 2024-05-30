f4631874f75afece28f89c3db75e4d9118b52603
Bernardo Ferrão <143981716+BernardoGFerrao@users.noreply.github.com>
2024-05-29 22:14:01 -0300

    Creating login and sign-up forms

--- fakepinterest/forms.py
+++ fakepinterest/forms.py
@@ -1,2 +1,27 @@
 #Onde vamos criar os formulários
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from fakepinterest.models import Usuario
 
+class FormLogin(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    botao_login = SubmitField('Fazer Login')
+
+class FormCriarConta(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    username = StringField('Usuario', validators=[DataRequired()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField('Confirme a senha',validators=[DataRequired(), EqualTo('senha')])
+    botao_criar_conta = SubmitField('Criar Conta')
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario:
+            return ValidationError("Email já cadastrado!")
+
+    def validate_username(self, username):
+        usuario = Usuario.query.filter_by(username=username.data).first()
+        if usuario:
+            return ValidationError("Usuário com username já cadastrado!")
