edf7001f4071e87a0ae7c678a1e3547d179ad27f
Bernardo Ferrão <143981716+BernardoGFerrao@users.noreply.github.com>
2024-05-30 17:27:37 -0300

    logging in and logging out the User class

--- fakepinterest/forms.py
+++ fakepinterest/forms.py
@@ -13,7 +13,7 @@ class FormCriarConta(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     username = StringField('Usuario', validators=[DataRequired()])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
-    confirmacao_senha = PasswordField('Confirme a senha',validators=[DataRequired(), EqualTo('senha')])
+    confirmacao_senha = PasswordField('Confirme a senha', validators=[DataRequired(), EqualTo('senha')])
     botao_criar_conta = SubmitField('Criar Conta')
 
     def validate_email(self, email):
--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -1,18 +1,37 @@
 #Onde vamos criar as rotas do site/links
-from flask import render_template, url_for
-from fakepinterest import app
-from flask_login import login_required
+from flask import render_template, url_for, redirect
+from fakepinterest import app, database, bcrypt
+from flask_login import login_required, login_user, logout_user, current_user
 from fakepinterest.forms import FormLogin, FormCriarConta
+from fakepinterest.models import Usuario, Post
 
 @app.route('/', methods=['GET', 'POST'])
 #Criando uma rota(Caminho para o link do site)( / -> Homepage)
 def homepage():
     formlogin = FormLogin()
+    if formlogin.validate_on_submit():#Se o usuário preencheu o login e está válido:
+        usuario = Usuario.query.filter_by(email=formlogin.email.data).first()
+        if usuario and bcrypt.check_password_hash(usuario.senha, formlogin.senha.data):#Se existe um usuário com esse email e a senha digitada no formulario coincide com a do db
+            login_user(usuario)
+            return redirect(url_for('perfil', usuario=usuario))
     return render_template('homepage.html', form=formlogin)
 
 @app.route('/criarconta', methods=['GET', 'POST'])
 def criarconta():
     formcriarconta = FormCriarConta()
+    if formcriarconta.validate_on_submit():#Se o usuário clicar no botão e as informações estiverem preenchidas:
+        #Criar um objeto do tipo Usuário:
+        senha = bcrypt.generate_password_hash(formcriarconta.senha.data)#Criptografa a senha do usuário
+        usuario = Usuario(username=formcriarconta.username.data,
+                          email=formcriarconta.email.data,
+                          senha=senha)
+        #Enviar o Usuário para o db:
+        database.session.add(usuario)
+        database.session.commit()
+        #Fazer o login do usuário:
+        login_user(usuario, remember=True)
+        #Redirecionar o usuário:
+        return redirect(url_for('perfil', usuario=usuario.username))
     return render_template('criarconta.html', form=formcriarconta)
 
 
@@ -22,3 +41,8 @@ def criarconta():
 def perfil(usuario):
     return render_template('perfil.html', usuario=usuario)#render template permite conversar com o html
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('homepage'))
