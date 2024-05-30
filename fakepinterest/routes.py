c4b48c897588a410f45a24db0de6e480612ac1e8
Bernardo Ferrão <143981716+BernardoGFerrao@users.noreply.github.com>
2024-05-30 00:51:42 -0300

    Creating a sign-up page and forms on the page

--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -2,14 +2,23 @@
 from flask import render_template, url_for
 from fakepinterest import app
 from flask_login import login_required
+from fakepinterest.forms import FormLogin, FormCriarConta
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 #Criando uma rota(Caminho para o link do site)( / -> Homepage)
 def homepage():
-    return render_template('homepage.html')
+    formlogin = FormLogin()
+    return render_template('homepage.html', form=formlogin)
+
+@app.route('/criarconta', methods=['GET', 'POST'])
+def criarconta():
+    formcriarconta = FormCriarConta()
+    return render_template('criarconta.html', form=formcriarconta)
+
 
 #Para não precisar criar uma rota para cada pessoa -> Páginas dinâmicas
 @app.route(f'/perfil/<usuario>')
 @login_required
 def perfil(usuario):
     return render_template('perfil.html', usuario=usuario)#render template permite conversar com o html
+
