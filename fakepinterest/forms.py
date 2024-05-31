237ec68357b34c294cf6cfbc2646df918713b2e8
Bernardo Ferrão <143981716+BernardoGFerrao@users.noreply.github.com>
2024-05-31 16:19:06 -0300

    validations for sign-up and login fields

--- fakepinterest/forms.py
+++ fakepinterest/forms.py
@@ -9,22 +9,27 @@ class FormLogin(FlaskForm):
     senha = PasswordField('Senha', validators=[DataRequired()])
     botao_login = SubmitField('Fazer Login')
 
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if not usuario:
+            raise ValidationError("Usuário inexistente!")
+
 class FormCriarConta(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     username = StringField('Usuario', validators=[DataRequired()])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
-    confirmacao_senha = PasswordField('Confirme a senha', validators=[DataRequired(), EqualTo('senha')])
+    confirmacao_senha = PasswordField('Confirme a senha', validators=[DataRequired(), EqualTo('senha', message="As senhas devem ser iguais!")])
     botao_criar_conta = SubmitField('Criar Conta')
 
     def validate_email(self, email):
         usuario = Usuario.query.filter_by(email=email.data).first()
         if usuario:
-            return ValidationError("Email já cadastrado!")
+            raise ValidationError("Email já cadastrado!")
 
     def validate_username(self, username):
         usuario = Usuario.query.filter_by(username=username.data).first()
         if usuario:
-            return ValidationError("Usuário com username já cadastrado!")
+            raise ValidationError("Usuário com username já cadastrado!")
 
 class FormFoto(FlaskForm):
     foto = FileField('Foto', validators=[DataRequired()])
